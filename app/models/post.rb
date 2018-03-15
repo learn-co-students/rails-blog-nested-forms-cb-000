f6f53425a3289d9be9d76db8ba5d4871697ad819
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-15 09:58:56 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-
+  #below allows tags to be nested in our new post form
+  accepts_nested_attributes_for :tags, reject_if: lambda {|attributes| attributes['name'].blank?}
   validates_presence_of :name, :content
 end
